9c921800e210e49f1de025305155553742cdc178
xiaoyelsk <1214719003@qq.com>
2018-05-31 16:03:47 +0800

    地址

--- src/components/users/address/address.vue
+++ src/components/users/address/address.vue
@@ -2,14 +2,32 @@
         <div id="site">
             <div class="Stop">
                 <ul class="StopUl">
-                    <li><i class="fa fa-bars"></i></li>
-                    <li><img src="http://static.21cake.com/themes/wap/img/logo.png" /></li>
+                    <li><router-link to="/users"><i class="fa fa-chevron-left"></i></router-link></li>
+                    <li></li>
                     <li><i class="fa fa-shopping-cart"></i></li>
                 </ul>
             </div>
             <div class="Smain">
-                <div class="kong">
+                <div class="kong" v-show="false">
                     <img src="//themes/wap/img/address-empty.png">
+                </div>
+                <div class="have">
+                    <ul>
+                        <li v-for="(item,idx) in address">
+                            <div class="hid">
+                                <h4>
+                                    {{item.receiver}}
+                                    <span>{{item.recephone}}</span>
+                                </h4>
+                                <div class="Dsite">
+                                    <span v-if="item.moren == 'true'">[默认地址]</span>
+                                    {{item.address}}({{item.minute}})
+                                </div>
+                                <i class="fa fa-pencil-square-o" aria-hidden="true" @click="upsite(item)"></i>
+                                <em @click="delsite(item)">删除</em>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div class="Sfoot">
@@ -22,14 +40,39 @@
     
     import './address.scss'
     import router from '../../../router/router'
+    import http from '../../../utils/httpclient.js'
 
     export default{
+        data(){
+            return {
+                address:[]
+            }
+        },
         components: {
         },
         methods:{
             Xsite(){
                 router.push({name:'addsite'});
+            },
+            upsite(item){
+                this.$router.push({name:'addsite',query:{_id:item._id}})
+            },
+            delsite(item){
+                let _id = item._id
+                http.post('delsite',{_id}).then((res) =>{
+                    console.log(res);
+                })
             }
+        },
+        mounted(){
+            let username = window.localStorage.getItem('username');
+
+            http.post('getaddress',{username}).then((res) =>{
+            
+                this.address = res.data
+
+                console.log(this.address)
+            })
         }
        
     }
