980c73ad213582ed6812c31a918d8f37a286aa13
方汉佳 <1690101402@qq.com>
2018-06-01 10:20:27 +0800

    修改图片路径

--- src/utils/httpclient.js
+++ src/utils/httpclient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import router from '../router/router.js'
 // 请求数据的端口号
-const baseUrl = 'http://10.3.133.73:88/'
+const baseUrl = 'http://10.3.133.250:88/'
 let filterUrl = (_url) => {
     if(_url && _url.startsWith('http')){
         return _url;
